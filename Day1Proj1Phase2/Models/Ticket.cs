01c6c67ca5976c9eb1af35679443154a551e4999
pradeep-krishnaa <pradeepkrishnaa.2205108@srec.ac.in>
2025-07-22 12:12:36 +0530

    feat:initial commit of Day 1 Project 1 Phase 2

---
+++ Day1Proj1Phase2/Models/Ticket.cs
@@ -0,0 +1,42 @@
+﻿using Day1Proj1Phase2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Proj1Phase2.Models
+{
+    public class Ticket
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; private set; }
+
+        public User CreatedBy { get; set; }
+        public Ticket(int id, string title, string desc, User createdBy)
+        {
+            Id = id;
+            Title = title;
+            Description = desc;
+            CreatedBy = createdBy;
+            Status = "Open"; // Default status when a ticket is created
+
+        }
+
+        public void CloseTicket()
+        {
+            Status = "Closed"; // Change status to Closed
+        }
+
+        public void DisplayTicket()
+        {
+            Console.WriteLine($"Ticket ID: {Id}");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Description : {Description}");
+            Console.WriteLine($"Status: {Status}");
+            Console.WriteLine($"Created By: {CreatedBy.Name} (Role: {CreatedBy.Role})");
+        }
+    }
+}
---
+++ Day1Proj1Phase2/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Proj1Phase2.Models
+{
+    public class User
+    {
+        //properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public User(int id, string name, string role)
+        {
+            Id = id;
+            Name = name;
+            Role = role;
+        }
+
+        //Method to Display User Info
+
+        public void DisplayUser()
+        {
+            Console.WriteLine($"User Name: {Name}");
+            Console.WriteLine($"User Role: {Role}");
+        }
+    }
+}
